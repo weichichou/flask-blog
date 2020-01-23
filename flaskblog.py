202e4d8b7ddd8a1d0c69e1d4415ecb053006b792
weichichou <weichi.cc@gmail.com>
2020-01-23 14:16:16 +0100

    Add templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+# List of dictionary
+posts = [
+    {
+        'author': 'Peter',
+        'title': 'Blog Post 1',
+        'content': 'Fist post content',
+        'date_posted': '23-01-2020'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '24-01-2020'
+    },
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title = 'about')
