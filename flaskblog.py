611e04d36639cb3408d7e9850f7ac16e243d2aac
weichichou <weichi.cc@gmail.com>
2020-01-23 17:02:05 +0100

    Add register form

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5k4g4au4au4'
 
 # List of dictionary
 posts = [
@@ -25,6 +28,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'about')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
