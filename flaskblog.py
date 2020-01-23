7e776dffdb1866839606acd2ead40287141c23fb
weichichou <weichi.cc@gmail.com>
2020-01-23 12:53:15 +0100

    Add About Page

--- flaskblog.py
+++ flaskblog.py
@@ -2,5 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+@app.route('/home')
+def home():
+    return '<h1>Home Page</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
